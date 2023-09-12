b466c62f4ff5b4a6b86b56224ffd963a21ba4f95
tshimegabelmont <tshimegabelmont@gmail.com>
2023-09-12 04:21:51 +0200

    Added Sign-In and Sign-Up routes and pages using clerk

--- frontend/app/(root)/page.tsx
+++ frontend/app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="p-4">
-      <Button>Click Me</Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
